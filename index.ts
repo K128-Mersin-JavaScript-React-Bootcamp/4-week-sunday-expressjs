f0033813caf30e471db70a65407e7b9592be160c
ZaferAyan <zaferayan@192.168.1.41>
2021-10-10 11:42:37 +0300

    Add heroku connection

--- index.ts
+++ index.ts
@@ -3,7 +3,19 @@ import pgPromise from 'pg-promise';
 const app = express();
 const pgp = pgPromise({});
 const port = 3001;
-var db = pgp("postgres://postgres:postgres@localhost:5432/postgres");
+
+//var db = pgp("postgres://postgres:postgres@localhost:5432/postgres");
+
+var db = pgp({
+  user:'xjzjvafpqfhesd',
+  password: 'b4f830de56079652337172c970846276accad62a05663a6b6647ee54856b4314',
+  host: 'ec2-52-214-178-113.eu-west-1.compute.amazonaws.com',
+  port: 5432,
+  database: 'de1mlrehih6jb5',
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 app.use(express.json());
 
@@ -18,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.name);
+  console.log(req.body);
   db.one(
     'INSERT INTO "MY_USERS" (name, surname) VALUES($1, $2) RETURNING id',
     [req.body.name, req.body.surname],
