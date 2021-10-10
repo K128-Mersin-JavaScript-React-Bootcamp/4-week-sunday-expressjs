b360b480fa2b9caf0c0e2ac0260160954e77d646
ZaferAyan <zaferayan@192.168.1.41>
2021-10-10 13:40:27 +0300

    Add jsdoc annotations

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ const usersRouter = require("./routes/users");
 const app = express();
 const pgp = pgPromise({});
 const port = 3001;
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -14,5 +16,28 @@ app.listen(process.env.PORT || port, () => {
 
 app.use(express.json());
 app.use("/", homeRouter);
+//app.use("/", hom);
 app.use("/birds", birdsRouter);
 app.use("/users", usersRouter);
+
+app.post("/login", (req, res) => {
+  console.log(req.url);
+});
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Hello World",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/*.ts"], // files containing annotations as above
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
--- routes/home.ts
+++ routes/home.ts
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-
 router.route("/").all((req: any, res: any, next: any) => {
   res.send("It works. Try different route");
 });
