9109f5e652144bd68206ec23356863ba8ddd6599
ZaferAyan <zaferayan@192.168.1.41>
2021-10-10 12:18:34 +0300

    Fix tsc build

--- build/index.js
+++ build/index.js
@@ -5,10 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const pg_promise_1 = __importDefault(require("pg-promise"));
+const dotenv = require('dotenv').config();
 const app = (0, express_1.default)();
 const pgp = (0, pg_promise_1.default)({});
 const port = 3001;
-var db = pgp("postgres://postgres:postgres@localhost:5432/postgres");
+//var db = pgp("postgres://postgres:postgres@localhost:5432/postgres");
+console.log(process.env.DB_USER);
+var db = pgp({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     db.query('SELECT id, name, surname FROM "MY_USERS"')
@@ -20,7 +32,7 @@ app.get("/", (req, res) => {
     });
 });
 app.post("/", (req, res) => {
-    console.log(req.body.name);
+    console.log(req.body);
     db.one('INSERT INTO "MY_USERS" (name, surname) VALUES($1, $2) RETURNING id', [req.body.name, req.body.surname], (event) => event.id).then((data) => {
         res.send(data);
     });
