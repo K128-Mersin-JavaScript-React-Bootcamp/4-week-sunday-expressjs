a32fd23da53f60297ff5cf3df94847ce92bf4f0c
ZaferAyan <zaferayan@192.168.1.41>
2021-10-10 13:00:17 +0300

    Add put and delete methods

--- index.ts
+++ index.ts
@@ -1,48 +1,18 @@
-import express from 'express';
-import pgPromise from 'pg-promise';
-const dotenv = require('dotenv').config();
+import express from "express";
+import pgPromise from "pg-promise";
+const dotenv = require("dotenv").config();
+const homeRouter = require("./routes/home");
+const birdsRouter = require("./routes/birds");
+const usersRouter = require("./routes/users");
 const app = express();
 const pgp = pgPromise({});
 const port = 3001;
 
-//var db = pgp("postgres://postgres:postgres@localhost:5432/postgres");
-
-console.log(process.env.DB_USER)
-
-var db = pgp({
-  user:process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  db.query('SELECT id, name, surname FROM "MY_USERS"')
-    .then(function (data) {
-      res.send(data);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  db.one(
-    'INSERT INTO "MY_USERS" (name, surname) VALUES($1, $2) RETURNING id',
-    [req.body.name, req.body.surname],
-    (event) => event.id
-  ).then((data) => {
-    res.send(data);
-  });
-});
-
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.use(express.json());
+app.use("/", homeRouter);
+app.use("/birds", birdsRouter);
+app.use("/users", usersRouter);
